fae94e9af65480c7c4c05b0ea747a16a83b1df96
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2024-05-19 00:10:26 +0900

    remove can not use constructor.

--- src/src-nompi/test_Matrix.cpp
+++ src/src-nompi/test_Matrix.cpp
@@ -12,115 +12,115 @@ public:
 
 void TestMatrix::run() {
 
-    Matrix<3, 3> m3_0(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0);
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_0(i, j), i * m3_0.sizeCol() + j);
-    m3_0.clear();
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_0(i, j), 0.0);
-    m3_0({0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0});
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_0(i, j), i * m3_0.sizeCol() + j);
+    Matrix<2, 2> m22_0 = {1.0, 2.0, 3.0, 4.0};
+    Matrix<3, 3> m33_0(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0);
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_0(i, j), i * m33_0.sizeCol() + j);
+    m33_0.clear();
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_0(i, j), 0.0);
+    m33_0({0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0});
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_0(i, j), i * m33_0.sizeCol() + j);
 
-    Matrix<3, 3> m3_1(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
-    Matrix<3, 3> m3_2 = m3_0 + m3_1;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_2(i, j), (m3_0(i, j) + m3_1(i, j)));
-    Matrix<3, 3> m3_3 = m3_0 - m3_1;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_3(i, j), (m3_0(i, j) - m3_1(i, j)));
-    Matrix<3, 3> m3_4 = m3_0 & m3_1;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_4(i, j), (m3_0(i, j) * m3_1(i, j)));
+    Matrix<3, 3> m33_1(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
+    Matrix<3, 3> m33_2 = m33_0 + m33_1;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_2(i, j), (m33_0(i, j) + m33_1(i, j)));
+    Matrix<3, 3> m33_3 = m33_0 - m33_1;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_3(i, j), (m33_0(i, j) - m33_1(i, j)));
+    Matrix<3, 3> m33_4 = m33_0 & m33_1;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_4(i, j), (m33_0(i, j) * m33_1(i, j)));
 
-    m3_2.clear();
-    m3_2 += m3_0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_2(i, j), (m3_0(i, j)));
-    m3_2.clear();
-    m3_2 -= m3_0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_2(i, j), (-m3_0(i, j)));
+    m33_2.clear();
+    m33_2 += m33_0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_2(i, j), (m33_0(i, j)));
+    m33_2.clear();
+    m33_2 -= m33_0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_2(i, j), (-m33_0(i, j)));
 
-    Matrix<3, 3> m3_5 = m3_0 + 1.0;
-    Matrix<3, 3> m3_6 = m3_0 - 1.0;
-    Matrix<3, 3> m3_7 = m3_0 * 1.0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_5(i, j), (m3_0(i, j) + 1.0));
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_6(i, j), (m3_0(i, j) - 1.0));
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_7(i, j), (m3_0(i, j) * 1.0));
+    Matrix<3, 3> m33_5 = m33_0 + 1.0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_5(i, j), (m33_0(i, j) + 1.0));
+    Matrix<3, 3> m33_6 = m33_0 - 1.0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_6(i, j), (m33_0(i, j) - 1.0));
+    Matrix<3, 3> m33_7 = m33_0 * 1.0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_7(i, j), (m33_0(i, j) * 1.0));
 
-    m3_0({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
-    m3_1({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
-    m3_0 += 2.0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_0(i, j), (m3_1(i, j) + 2.0));
-    m3_0({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
-    m3_0 -= 2.0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_0(i, j), (m3_1(i, j) - 2.0));
-    m3_0({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
-    m3_0 *= 2.0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_0(i, j), (m3_1(i, j) * 2.0));
+    m33_0({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
+    m33_1({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
+    m33_0 += 2.0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_0(i, j), (m33_1(i, j) + 2.0));
+    m33_0({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
+    m33_0 -= 2.0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_0(i, j), (m33_1(i, j) - 2.0));
+    m33_0({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
+    m33_0 *= 2.0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_0(i, j), (m33_1(i, j) * 2.0));
 
     
-    m3_0({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
-    Matrix<3, 3> m3_8 = -m3_0; 
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_8(i, j), (m3_0(i, j) * (-1.0)));
-    Matrix<3, 3> m3_9 = 1.0 + m3_0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_9(i, j), (m3_0(i, j) + 1.0));
-    Matrix<3, 3> m3_10 = 1.0 - m3_0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_10(i, j), (-m3_0(i, j) + 1.0));
-    Matrix<3, 3> m3_11 = 1.0 * m3_0;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m3_11(i, j), m3_0(i, j));
+    m33_0({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
+    Matrix<3, 3> m33_8 = -m33_0; 
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_8(i, j), (m33_0(i, j) * (-1.0)));
+    Matrix<3, 3> m33_9 = 1.0 + m33_0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_9(i, j), (m33_0(i, j) + 1.0));
+    Matrix<3, 3> m33_10 = 1.0 - m33_0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_10(i, j), (-m33_0(i, j) + 1.0));
+    Matrix<3, 3> m33_11 = 1.0 * m33_0;
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_11(i, j), m33_0(i, j));
 
-    m3_1({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
-    Matrix<3, 3> m33_12 = ~m3_1; 
-    std::cout << m3_1 << std::endl;
+    // transpose
+    m33_1({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
+    Matrix<3, 3> m33_12 = ~m33_1; 
+    std::cout << m33_1 << std::endl;
     std::cout << m33_12 << std::endl;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m33_12(i, j), m3_0(j, i));
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_12(i, j), m33_0(j, i));
     !m33_12;
+    std::cout << "m33_12" << std::endl;
     std::cout << m33_12 << std::endl;
-    for (size_t i = 0; i < m3_0.sizeRow(); i++)
-        for (size_t j = 0; j < m3_0.sizeCol(); j++)
-            test_double_equals(m33_12(i, j), m3_0(i, j));
+    for (size_t i = 0; i < m33_0.sizeRow(); i++)
+        for (size_t j = 0; j < m33_0.sizeCol(); j++)
+            test_double_equals(m33_12(i, j), m33_0(i, j));
 
-    // Matrix<4, 3> m43_0;
-    // m43_0.clear();
-    // !m43_0;
-    
+    // Matrix and Vector multiplication.
     Matrix<4, 3> m43_0;
-    Array<3> arr3_0;
     for (std::size_t i = 0; i < m43_0.sizeRow(); i++) 
         for (std::size_t j = 0; j < m43_0.sizeCol(); j++)
             m43_0(i, j) = i * m43_0.sizeCol() + j;
+    Array<3> arr3_0;
     for (std::size_t i = 0; i < arr3_0.size(); i++)
         arr3_0[i] = i;
     std::cout << m43_0 << std::endl;
@@ -134,7 +134,8 @@ void TestMatrix::run() {
         }
         test_double_equals(arr4_0[i], sum);
     }
-
+    
+    // Vector and Matrix multiplication.
     Array<3> arr3_1;
     for (std::size_t i = 0; i < arr3_1.size(); i++)
         arr3_1[i] = i;
@@ -153,7 +154,6 @@ void TestMatrix::run() {
         }
         test_double_equals(arr4_1[j], sum);
     }
-        
 }
 
 int main(int argc, char *argv[]) {
