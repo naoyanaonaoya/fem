02f22821aa7a5a2e908ff53ddb3cf2f15999499c
naoyanaonaoya <naoya.basket.a.i@gmail.com>
2024-06-10 03:41:27 +0900

    add

--- src/src-nompi/main.cpp
+++ src/src-nompi/main.cpp
@@ -1,10 +1,44 @@
+#include "CfdDriver_sp.hpp"
+
 #include <iostream>
-#include "ArrayXYZ.hpp"
-#include "ElementTri.hpp"
-#include "ElementQuad.hpp"
-#include "Array.hpp"
-#include "Matrix.hpp"
-#include "Node.hpp"
+
+void PrintUsage() {
+    std::cerr << "Usage : main casefile [-r]\n";
+    std::cerr << " -r : restart mode\n";
+}
+
+class Abmac2DSpArgs {
+
+public:
+
+    std::string case_file_name_;
+
+    bool is_restart_mode_;
+
+    bool analyze(int argc, char* argv[]) {
+        case_file_name_ = "";
+        is_restart_mode_ = false;
+        for (std::size_t i = 1; i < argc; i++) {
+            std::string str = argv[i];
+            if (str == "-r")
+                is_restart_mode_ = true;
+            else {
+                if (case_file_name_ == "")
+                    case_file_name_ = str;
+                else {
+                    PrintUsage();
+                    return false;
+                }
+            }
+        }
+        if (case_file_name_ == "") {
+            PrintUsage();
+            return false;
+        }
+        return true;
+    }
+
+};
 
 /**
  * @fn
@@ -14,116 +48,51 @@
  * @return int
  */
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, world!\n";
 
-    Logger::openLog("./test", 0);
-    Logger::out << "test\n";
-    Logger::out << "hello world\n";
-    Logger::closeLog();
+    CfdDriverSp driver_sp;
+    std::size_t rank = 0;
+    std::size_t num_procs = 1;
 
-    ArrayXYZ v1(1.0, 2.0, 3.0);
-    std::cout << v1 << std::endl;
-    ArrayXYZ v2(4.0, 5.0, 6.0);
-    std::cout << v2 << std::endl;
-    ArrayXYZ v3 = v1 + v2;
-    std::cout << v3 << std::endl;
+    Abmac2DSpArgs args;
+    if (!args.analyze(argc, argv))
+        return EXIT_FAILURE;
 
-    Array<3> a3_1(1.0, 2.0, 3.0);
+    try {
 
-    Array<4> a4_1(4.0, 3.0, 2.0, 1.0);
-    std::cout << "========\n";
-    Array<4> a4_2;
-    std::cout << a4_2 << std::endl;
-    a4_1 = a4_1;
-    a4_2.clear();
-    a4_2 = a4_1;
-    std::cout << a4_2 << std::endl;
-    std::cout << "========\n";
-    
-    // Array<3> a3_2 = a3_1;
-    // Array<4> a4_3 = a3_1;
+        driver_sp.Init(num_procs, rank, args.case_file_name_.c_str());
+        Params* params = driver_sp.GetParams();
+        params->is_restart_mode_ = args.is_restart_mode_;
+        params->is_single_process_mode_ = true;
+        driver_sp.ReadDataFile();
+        driver_sp.CalcInvariants();
+        driver_sp.SetupFieldData();
 
-    std::cout << "========\n";
-    a4_2 = a4_1 + a4_1;
-    std::cout << a4_2 << std::endl;
-    std::cout << "========\n";
+        driver_sp.PrintCalcCondition();
+        /*
+         * 時間発展ループ
+         */
+        // IMPLEMENT ME
+        // while (driver_sp.getState()->getT() <= params->duration_) {
+        while (params->shouldContinue(driver_sp.GetState())) {
+            driver_sp.PrintState();
+            driver_sp.DoStep();
+            driver_sp.PrintCalcData();
+        }
 
-    std::cout << "test0" << std::endl;
+        /*
+         * 後処理
+         */
+        driver_sp.Finalize();
 
 
-    // ElementTri t1;
-    ElementQuad q1;
-    std::vector<Node*> nodes3;
-    std::vector<Node*> nodes4;
-    ArrayXYZ v4(1.0, 2.0, 3.0);
-    Node node0 = Node(v4);
-    for (int i = 0; i < 3; i++) {
-        nodes3.push_back(&node0);
+    } catch (DataException &exp) {
+        std::cerr << exp << "\n";
+        return EXIT_FAILURE;
+    } catch (IoException &exp) {
+        std::cerr << exp << "\n";
+        return EXIT_FAILURE;
     }
-    for (int i = 0; i < 4; i++) {
-        nodes4.push_back(&node0);
-    }
-    std::cout << "test1" << std::endl;
-    // t1.setNodes(nodes3);
-    q1.setNodes(nodes4);
-    // ElementQuad e1(0, 1, 2, 3);
 
-    Matrix<4, 4> m44_0;
-    Matrix<3, 2> m32_0;
-    Matrix<2, 1> m21_0;
-    m44_0.clear();
-    m32_0.clear();
-    m21_0.clear();
-    for (std::size_t i = 0; i < m44_0.sizeRow(); i++)
-        for (std::size_t j = 0; j < m44_0.sizeCol(); j++)
-            m44_0(i, j) = i * m44_0.sizeCol() + j;
-    for (std::size_t i = 0; i < m32_0.sizeRow(); i++)
-        for (std::size_t j = 0; j < m32_0.sizeCol(); j++)
-            m32_0(i, j) = i * m32_0.sizeCol() + j;
-    for (std::size_t i = 0; i < m21_0.sizeRow(); i++)
-        for (std::size_t j = 0; j < m21_0.sizeCol(); j++)
-            m21_0(i, j) = i * m21_0.sizeCol() + j;
-
-    std::cout << "test0" << std::endl;
-
-    Matrix<4, 4> m44_1;
-    m44_1.clear();
-    for (std::size_t i = 0; i < m44_1.sizeRow(); i++)
-        for (std::size_t j = 0; j < m44_1.sizeCol(); j++)
-            m44_1(j, i) = i * m44_1.sizeCol() + j;
-
-    std::cout << "m44_0" << std::endl;
-    std::cout << m44_0 << std::endl;
-    std::cout << "m44_1" << std::endl;
-    std::cout << m44_1 << std::endl;
-    Matrix<4, 4> m44_2 = m44_0 + m44_1;
-    std::cout << "m44_2" << std::endl;
-    std::cout << m44_2 << std::endl;
-
-    Matrix<4, 4> m44_3 = -m44_0;
-    std::cout << "m44_3" << std::endl;
-    std::cout << m44_3 << std::endl;
-
-    Matrix<4, 3> m43_0;
-    for (std::size_t i = 0; i < m43_0.sizeRow(); i++)
-        for (std::size_t j = 0; j < m43_0.sizeCol(); j++)
-            m43_0(i, j) = i * m43_0.sizeCol() + j;
-    std::cout << "m43_0" << std::endl;
-    std::cout << m43_0 << std::endl;
-    Matrix<4, 3> m43_1 = m44_0 * m43_0;
-    std::cout << "m43_1" << std::endl;
-    std::cout << m43_1 << std::endl;
-    // Matrix<4, 2> m43_2 = m44_0 * m32_0;
-    // std::cout << "m43_2" << std::endl;
-    // std::cout << m43_2 << std::endl;
-
-    // this code should make error at the compile timing!!
-    // m44_2 = m44_0 + m32_0;
-    // std::cout << m44_2 << std::endl;
-    
-    Matrix<4, 2> m42_0(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0);
-    std::cout << m42_0 << std::endl;
-
-    return 0;
+    return EXIT_SUCCESS;
 }
 
